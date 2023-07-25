944f4be558dd619c29f97980e9608d1eb71f462f
IanPomaz <128009980+IanPomaz@users.noreply.github.com>
2023-07-24 22:32:30 -0400

    delete category.routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,6 +36,9 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({where:{id:req.params.id}}).then(data=>{
+    res.json(data)
+  })
 });
 
 module.exports = router;
